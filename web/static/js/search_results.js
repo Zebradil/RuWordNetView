941a5946b77366068e3bf6cb90ff213c9fab9c52
German Lashevich <german.lashevich@gmail.com>
2019-03-18 00:15:21 +0100

    Restyling + nojs

--- web/static/js/search_results.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('#meanings').change(function () {
-        var synsetId = $(this).val();
-        $('.result-item').hide().filter('[data-synsetId="' + synsetId + '"]').show();
-    });
-});
